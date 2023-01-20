fc73de746329a7714da3c7c916d149ea322f8b55
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-01-20 14:35:49 +0500

    final commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 app.get('/', (req, res) => {
-    res.send("build docker image with jenkins by hakym yar beyg")
+    res.send("build docker image with jenkins")
 });
 app.listen( PORT, () => console.log("Server is listening to port" + PORT ));
